40b82bd9e3272cba033aa1c5d13f70173786fce6
Benjamin Fuks <fuks@lpthe.jussieu.fr>
2019-09-08 20:09:57 +0200

    adding connection to steem; issues with browser storage

--- client/main.js
+++ client/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-
 import App from '/imports/ui/App.vue'
 
 //Meteor.startup(() => {
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,29 +1,60 @@
 <template>
-<div>
-  <h1>{{ greetings }} </h1>
+  <div>
+    <h1>{{ greetings }} </h1>
 
-  <button @click='count()'> Click me </button>
+    <button @click='count()'> Click me </button>
 
-  <p>You have clicked {{counter }} times.</p>
+    <p>You have clicked {{counter }} times.</p>
 
-  <p>{{steem_props()}} </p>
-</div>
+    <p>Steem properties: {{steem_props()}} </p>
+  </div>
 </template>
 
 <script>
-  import APIConnect from '/imports/steem/ApiConnect.js';
+  global.Buffer = global.Buffer || require("buffer").Buffer;
+  import steem from 'steem';
+
+  // Main function to connect to the Steem API
+  function APIConnect(API_counter=0)
+  {
+    // Local variables
+    let API_list = ['https://api.steemit.com', 'https://api.steem.house', 'https://anyx.io'];
+
+    // Safety
+    if(API_counter>=API_list.length) { console.log('  --> cannot find a working node. Exiting...'); return; }
+    let api = API_list[API_counter];
+
+    // Preparing the request
+    let SendDate = (new Date()).getTime();
+    console.log('Trying to connect to the Steem API:', api);
+    steem.api.setOptions({url:api});
+
+    // Making the request
+    steem.api.getDynamicGlobalProperties(function(error, result)
+    {
+      // Safety
+      if(!result)
+        { console.log('  --> Cannot connect to the ' + api + ' API:', error); APIConnect(APIcounter+1); return; }
+
+      // Getting the API answer
+      let ReceiveDate = (new Date()).getTime();
+      console.log('  --> Global properties loaded from', api, 'in', (ReceiveDate - SendDate), 'ms');
+
+      // Saving global properties
+      localStorage.setItem('SteemProperties', JSON.stringify(result));
+    });
+  }
 
   // Getting Steem global properties
-  localStorage.setItem('Steem_properties', '');
+  APIConnect();
 
   export default
   {
     data()   { return { greetings: 'Super mouth!', counter  : 0 }; },
-    mounted(){ APIConnect() },
     methods:
     {
-      count()     { this.counter++; },
-      steem_props() { return JSON.stringify(localStorage.getItem('Steem_properties')); }
+      count()       { this.counter++; },
+      steem_props() { return localStorage.SteemProperties; }
     }
   };
 </script>
